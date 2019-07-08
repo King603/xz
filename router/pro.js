e3014eeaeda1de0d124a2bcde987378d4f660c62
King <408385413@example.com>
2019-07-08 19:36:46 +0800

    学习

--- router/pro.js
+++ router/pro.js
@@ -20,6 +20,14 @@ router.get("/V1/login/:uname&:upwd",(req,res)=>{
 	});
 });
 //2.用户列表
+router.get('/V1/alluserlist',(req,res)=>{
+	//执行sql语句
+	pool.query('select * from xz_user',(err,result)=>{
+		if(err)throw err;
+		res.send(result);
+	});
+});
+//3.用户列表分页查询
 router.get('/V1/list/:count&pno',(req,res)=>{
 	//获取数据
 	var obj=req.params;
@@ -32,6 +40,48 @@ router.get('/V1/list/:count&pno',(req,res)=>{
 	pool.query('select * from xz_user limit ?,?',[$start,$count],(err,result)=>{
 		if(err)throw err;
 		res.send(result);
+	});
+});
+//4.根据uid删除用户
+router.delete("/V1/deluser/:uid",(req,res)=>{
+	var $uid=req.params.uid;
+	if(!$uid){res.render("未找到");return;}
+	pool.query("delete from xz_user where uid=?",[$uid],(err,result)=>{
+		if(err)throw err;
+		if(result.affecterRows>0)res.send("1");
+		else res.send("0");
+	});
+});
+//5.根据uid查询用户
+router.get("/V1/queryuser/:uid",(req,res)=>{
+	var $uid=req.params.uid;
+	//查询数据库，返回响应
+	var sql='select * from xz_user where uid=?';
+	pool.query(sql,[$uid],(err,result)=>{
+		if(err)throw err;
+		if(result.length>0)res.send(result);
+		else res.send("0");
+	});
+});
+//6.修改用户信息
+router.put("/V1/update",(req,res)=>{
+	var obj=req.body;
+	var $uid=obj.uid;
+	delete obj.uid;
+	var sql="update xz_user set ?where uid=?";
+	pool.query(sql,[obj,$uid],(err,result)=>{
+		if(err)throw err;
+		res.send("1");
+	});
+});
+//7.用户注册
+router.post("/V1/reg",(req,res)=>{
+	//获取数据
+	var obj=req.body;
+	//执行SQL语句
+	pool.query('insert into xz_user set ?',[obj],(err,result)=>{
+		if(err)throw err;
+		if(result.affectedRows>0)res.send({code:200,msg:'reg suc'});
 	});
 });
 //导出路由器对象
